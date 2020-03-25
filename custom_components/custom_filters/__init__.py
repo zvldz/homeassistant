e9f100f990d03d8836309e9757ce3959facdbbdf
zvldz <zvldz@users.noreply.github.com>
2020-03-25 09:10:45 +0200

    [custom_components/custom_filters] Adaptation to version 0.107+

--- custom_components/custom_filters/__init__.py
+++ custom_components/custom_filters/__init__.py
@@ -1,56 +1,54 @@
 """Support custom filters for jinja"""
 import urllib.parse
-import hashlib
 
 from random import Random, SystemRandom, shuffle
 
 from homeassistant.helpers import template
 
-DOMAIN = 'custom_filters'
 
+_TemplateEnvironment = template.TemplateEnvironment
 
-async def async_setup(hass, config):
-    _TemplateEnvironment = template.TemplateEnvironment
+def unquote(string):
+    return urllib.parse.unquote(string)
 
-    def unquote(string):
-        return urllib.parse.unquote(string)
+def ternary(value, true_val, false_val, none_val=None):
+    '''  value ? true_val : false_val '''
+    if value is None and none_val is not None:
+        return none_val
+    elif bool(value):
+        return true_val
+    else:
+        return false_val
 
-    def ternary(value, true_val, false_val, none_val=None):
-        '''  value ? true_val : false_val '''
-        if value is None and none_val is not None:
-            return none_val
-        elif bool(value):
-            return true_val
+def randomize_list(mylist, seed=None):
+    try:
+        mylist = list(mylist)
+        if seed:
+            r = Random(seed)
+            r.shuffle(mylist)
         else:
-            return false_val
-
-    def randomize_list(mylist, seed=None):
-        try:
-            mylist = list(mylist)
-            if seed:
-                r = Random(seed)
-                r.shuffle(mylist)
-            else:
-                shuffle(mylist)
-        except Exception:
-            pass
-        return mylist
+            shuffle(mylist)
+    except Exception:
+        pass
+    return mylist
 
 
-    def init(*args):
-        env = _TemplateEnvironment(*args)
+def init(*args):
+    env = _TemplateEnvironment(*args)
 
-        env.filters['unquote'] = unquote
-        env.filters['urldecode'] = unquote
-        env.filters['ternary'] = ternary
-        env.filters['shuffle'] = randomize_list
+    env.filters['unquote'] = unquote
+    env.filters['urldecode'] = unquote
+    env.filters['ternary'] = ternary
+    env.filters['shuffle'] = randomize_list
 
-        return env
+    return env
 
-    template.TemplateEnvironment = init
-    template._NO_HASS_ENV.filters['unquote'] = unquote
-    template._NO_HASS_ENV.filters['urldecode'] = unquote
-    template._NO_HASS_ENV.filters['ternary'] = ternary
-    template._NO_HASS_ENV.filters['shuffle'] = randomize_list
+template.TemplateEnvironment = init
+template._NO_HASS_ENV.filters['unquote'] = unquote
+template._NO_HASS_ENV.filters['urldecode'] = unquote
+template._NO_HASS_ENV.filters['ternary'] = ternary
+template._NO_HASS_ENV.filters['shuffle'] = randomize_list
 
+
+def setup(hass, hass_config):
     return True

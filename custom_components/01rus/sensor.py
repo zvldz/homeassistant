b4556657c4d4b3c77b62941650844f4003bd758b
zvldz <zvldz@users.noreply.github.com>
2020-06-15 13:05:53 +0300

    update 2020/06/15

--- custom_components/01rus/sensor.py
+++ custom_components/01rus/sensor.py
@@ -1,8 +1,7 @@
-"""Support for the 01rus."""
+"""Support for the 01rus.ru."""
 from datetime import timedelta, date
 from bs4 import BeautifulSoup
 import logging
-#import time
 
 import requests
 import voluptuous as vol
@@ -13,6 +12,7 @@ from homeassistant.const import (
     CONF_NAME,
     CONF_USERNAME,
     CONF_PASSWORD,
+    HTTP_OK,
 )
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
@@ -20,7 +20,7 @@ from homeassistant.helpers.entity import Entity
 _LOGGER = logging.getLogger(__name__)
 
 ATTRIBUTION = "Data provided by 01rus.ru"
-DEFAULT_NAME = " 01rus sensor"
+DEFAULT_NAME = " 01rus.ru sensor"
 SCAN_INTERVAL = timedelta(seconds=3600)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
@@ -37,17 +37,13 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
 
-    result = prov01rusSensor(name, username, password)
+    result = site01rusSensor(name, username, password)
     result.update()
-
-#   if result.data.get("error") == "111":
-#       _LOGGER.error("data not available")
-#       return
 
     add_entities([result])
 
 
-class prov01rusSensor(Entity):
+class site01rusSensor(Entity):
 
     def __init__(self, name, username, password):
         """Initialize the sensor."""
@@ -64,12 +60,22 @@ class prov01rusSensor(Entity):
     @property
     def device_state_attributes(self):
         """Return the state attributes of this device."""
+        if self.data is None:
+            _LOGGER.error("Unable to retrieve data")
+            return
+
         attr = {ATTR_ATTRIBUTION: ATTRIBUTION}
         raw_data = BeautifulSoup(self.data.text, "html.parser")
         _LOGGER.debug(raw_data)
 
-        attr['up'] = round(float(raw_data.find_all('td')[9].get_text()), 2)
-        attr['down'] = round(float(raw_data.find_all('td')[5].get_text()), 2)
+        traff_in = raw_data.find('td', text='Входящий').findNextSibling('td')
+        traff_out = raw_data.find('td', text='Исходящий').findNextSibling('td')
+
+        if traff_in != None and traff_out != None:
+            attr['down'] = round(float(traff_out.get_text()), 2)
+            attr['up'] = round(float(traff_out.get_text()), 2)
+        else:
+            _LOGGER.warning("Result could not be parsed")
 
         return attr
 
@@ -81,18 +87,26 @@ class prov01rusSensor(Entity):
         return None
 
     def update(self):
-        """Get the latest data from 01rus."""
+        """Get the latest data from 01rus.ru."""
         url = 'http://abonent.01rus.ru/'
+        request_data = {
+            'username': self._username,
+            'password': self._password,
+        }
 
         try:
             session = requests.Session()
-
-            request_data = {
-                'username': self._username,
-                'password': self._password,
-            }
-
             response = session.post(url, data=request_data, headers=dict(Referer=url))
+
+            if response.status_code != HTTP_OK:
+                _LOGGER.error("Error %d on load URL %s", request.status, request.url)
+                self.data = None
+                return None
+
+            if response.url.find('user') == -1:
+                _LOGGER.error("Failed to login to %s", url)
+                self.data = None
+                return None
 
             start_date = date.today().strftime("01.%m.%Y")
             end_date = date.today() + timedelta(days=1)
@@ -106,5 +120,5 @@ class prov01rusSensor(Entity):
             self.data = session.post(url + 'user/traffic', data=request_data)
             _LOGGER.debug("Data fetched from resource: \n%s", self.data.text)
         except ValueError as err:
-            _LOGGER.error("Error retrieving data from 01rus.ru: %s", err.args)
+            _LOGGER.error("Error retrieving data from %s: %s", url, err.args)
             self.data = None
